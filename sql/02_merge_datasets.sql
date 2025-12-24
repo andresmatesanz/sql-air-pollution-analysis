e089e19d4559149663376414826e11d30b1d8ff6
Andrés Matesanz <andresmatesanz@users.noreply.github.com>
2025-12-24 01:46:34 +0100

    Mergear las tablas anuales en la tabla principal t_master

---
+++ sql/02_merge_datasets.sql
@@ -0,0 +1,9 @@
+-- Merge de las tablas anuales en una tabla principal t_master
+CREATE TABLE t_master AS
+SELECT * FROM emisiones2020
+UNION ALL
+SELECT * FROM emisiones2021t_master
+UNION ALL
+SELECT * FROM emisiones2022
+UNION ALL
+SELECT * FROM emisiones2023;
